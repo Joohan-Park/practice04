20d778c1254b9ec607d525b37c69d222517ffd02
joohanpark <wngksqkr91@naver.com>
2016-09-20 15:27:48 +0900

    과제

--- src/prob1/StringUtilTest.java
+++ src/prob1/StringUtilTest.java
@@ -4,7 +4,8 @@ public class StringUtilTest {
 
 	public static void main(String[] args) {
         String[] strArr = { "SuperMan", "BatMan", "SpiderMan" };
-        String resultStr = concatenate( strArr );
+        System.out.println(strArr.length);
+        String resultStr = StringUtil.concatenate( strArr );
         System.out.println( "결과 문자열 : " + resultStr ); 
 	}
 
--- src/prob2/Printer.java
+++ src/prob2/Printer.java
@@ -1,4 +1,52 @@
 package prob2;
 
 public class Printer {
+	boolean b;
+	char c;
+	byte by;
+	short s;
+	int i;
+	long l;
+	float f;
+	double d;
+	String st;
+	
+	public void println(boolean b){
+		this.b=b;
+		System.out.println(b);
+	}
+	public void println(char c){
+		this.c=c;
+		System.out.println(c);
+	}
+	public void println(byte by){
+		this.by=by;
+		System.out.println(by);
+	}
+	public void println(short s){
+		this.s=s;
+		System.out.println(s);
+	}
+	public void println(int i){
+		this.i=i;
+		System.out.println(i);
+	}
+	public void println(long l){
+		this.l=l;
+		System.out.println(l);
+	}
+	public void println(float f){
+		this.f=f;
+		System.out.println(c);
+	}
+	public void println(double d){
+		this.d=d;
+		System.out.println(d);
+	}
+	public void println(String st){
+		this.st=st;
+		System.out.println(st);
+	}
+	
+	
 }
